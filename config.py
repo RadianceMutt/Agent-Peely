e5f4f97d4d756d50890fbb4e849c1b82f803dd94
The Virtuality System <60554183+RadianceMutt@users.noreply.github.com>
2023-05-17 19:37:10 +0100

    added the config file.

---
+++ config.py
@@ -0,0 +1,7 @@
+from discord import Intents
+from discord import __version__
+
+discord_token = 'DISCORD-BOT-TOKEN'
+library_version = __version__
+version = '0.0.2-pre_beta'
+configured_intents = Intents.all()
