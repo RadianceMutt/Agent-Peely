d1626a13e5c44085d537167c8c9e1b9f714a948f
RadianceMutt <60554183+RadianceMutt@users.noreply.github.com>
2023-06-11 16:22:55 +0100

    Update config.py
    better formatted config file.

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 from discord import Intents
 from discord import __version__
+from discord import Colour as colour
 
-discord_token = 'DISCORD-BOT-TOKEN'
+discord_token = 'ODczNDgwMzE5OTM5MDAyNDA4.GN5eGf.ek0-pYBU7nfB16d6kFbwph08aGyHelpSWnjX9A'
+discord_colour = colour.yellow()
+
+fortnite_token = '01e7010a-7e19-4b7f-b920-8c3a3f141674'
+fortnite_language = "en"
+
+ip_address = "151.228.27.72/32"
+username = "mastertom_discordbot0210"
+password = "masterDISCORD2023"
+client_url = f"mongodb+srv://{username}:{password}@mastertombot.vtfgypy.mongodb.net/?retryWrites=true&w=majority"
+
 library_version = __version__
-version = '0.0.2-pre_beta'
+version = '1.0.1-beta'
 configured_intents = Intents.all()
+moderation_log_id = 1104806128753311764
